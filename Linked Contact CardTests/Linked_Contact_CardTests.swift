4a081d585f0214fd892a0e94b92cc176afd236ba
Ben <benduke.nz@gmail.com>
2017-05-25 15:43:27 +1200

    Initial Commit

---
+++ Linked Contact CardTests/Linked_Contact_CardTests.swift
@@ -0,0 +1,36 @@
+//
+//  Linked_Contact_CardTests.swift
+//  Linked Contact CardTests
+//
+//  Created by Ben Duke on 25/05/17.
+//  Copyright © 2017 Ben Duke. All rights reserved.
+//
+
+import XCTest
+@testable import Linked_Contact_Card
+
+class Linked_Contact_CardTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
